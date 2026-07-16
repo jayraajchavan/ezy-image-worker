8224fa3e9285443fc37f04660069aa498fb50df1
Jayraaj Chavan <jayraaj.chavan@gmail.com>
2026-07-16 08:27:18 +0530

    Add worker metadata

--- app/version.py
+++ app/version.py
@@ -1,2 +1,23 @@
-VERSION = "1.0.0"
-WORKER_NAME = "FLUX2 Klein RunPod Worker"
+"""
+ezy-image-worker
+Version information for the RunPod Serverless Image Worker.
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class WorkerInfo:
+    """Static metadata describing this worker."""
+
+    name: str = "ezy-image-worker"
+    version: str = "1.0.0"
+    description: str = (
+        "Generic AI Image Generation Worker for the eZy ecosystem."
+    )
+    author: str = "Jayraaj Chavan"
+    license: str = "Apache-2.0 Compatible Worker"
+    default_model: str = "black-forest-labs/FLUX.2-klein-4B"
+
+
+WORKER_INFO = WorkerInfo()
